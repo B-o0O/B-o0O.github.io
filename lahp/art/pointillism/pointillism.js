07248273ca1857a2b1ff9d940efa47ea796120f1
Bobo <3327943945@qq.com>
2025-03-10 00:47:19 +0800

    Site updated: 2025-03-10 00:47:19

---
+++ lahp/art/pointillism/pointillism.js
@@ -0,0 +1,24 @@
+console.log("进入界面")
+window.onload = (event) => {
+  console.log("页面已完全加载");
+
+//   let main = document.getElementsByClassName("post-block")[0];
+  let main = document.getElementsByClassName("main-inner")[0];
+  let column = document.getElementsByClassName("column")[0];
+  console.log(column.style)
+  column.style.display = "none";
+  console.log(main);
+main.classList.add("pointillism");
+
+    console.log(main);
+};
+
+
+    let time=null;
+
+time = setInterval(()=>{
+            console.log(document.readyState);
+            if(document.readyState == "complete"){
+                console.log("页面已完全加载");
+                clearInterval(time);}
+        },0.1)
